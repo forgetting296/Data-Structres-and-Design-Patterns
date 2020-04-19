24f48d0e9d627e1fac1f10c8df20bbcc98797874
liuzp <1131664758@qq.com>
2020-04-19 20:43:37 +0800

    review leetcode

---
+++ src/main/java/com/shusaku/study/leetcode/HuaWeiTest.java
@@ -0,0 +1,10 @@
+package com.shusaku.study.leetcode;
+
+/**
+ * @program: Data Structures And Design Patterns
+ * @description:
+ * @author: Shusaku
+ * @create: 2020-04-19 14:04
+ */
+public class HuaWeiTest {
+}
